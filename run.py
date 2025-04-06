263469e90792944005e5b6b5b591461b710e2b43
Rifqi <rifqiramadani16@gmail.com>
2025-04-06 23:16:30 +0700

    c

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'revobank_secret_key')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-        'DB_CONNECTION_STRING', 'postgresql://postgres:postgres@localhost:5432/revobank?sslmode=require')
+        'DATABASE_URL', 'postgresql://postgres:postgres@localhost:5432/revobank?sslmode=require')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
         'pool_pre_ping': True
